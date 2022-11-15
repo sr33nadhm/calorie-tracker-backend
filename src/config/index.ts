2f7e26d6a9254ddb875b5c8f94ae8ace5f9f6125
Sreenadh M <sreenathpurushothaman@gmail.com>
2022-11-15 16:29:03 +0530

    Updated port config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 enum NodeEnv {
-  TEST = 'test',
-  DEV = 'development',
+  TEST = "test",
+  DEV = "development",
 }
 
 interface Env {
@@ -19,10 +19,10 @@ interface Env {
 
 export const config: Env = {
   env: (process.env.NODE_ENV as NodeEnv) || NodeEnv.DEV,
-  dbFilename: process.env.DB_FILENAME || '',
-  dbTestFilename: process.env.DB_TEST_FILENAME || '',
-  knexDebug: process.env.KNEX_DEBUG === 'true',
-  port: process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 5000,
+  dbFilename: process.env.DB_FILENAME || "",
+  dbTestFilename: process.env.DB_TEST_FILENAME || "",
+  knexDebug: process.env.KNEX_DEBUG === "true",
+  port: parseInt(process.env.APP_PORT) || 5000,
   defaultPage: 0,
   defaultPageSize: 10,
 };
