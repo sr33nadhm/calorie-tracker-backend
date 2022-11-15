2d05e72aed5d3d01886464889b5d9987f38d0d45
Sreenadh M <sreenathpurushothaman@gmail.com>
2022-11-15 09:54:52 +0530

    Updated build and run

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import app from './app';
-import { config } from './config';
+import app from "./app";
+import { config } from "./config";
 
 app.listen(config.port, () => {
   console.log(`App is running on port ${config.port}`);
